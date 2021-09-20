d4ea32f69d23aee26045e5da286e7a0ddb5cd08d
Eric Rogers <Rogers.eric2012@gmail.com>
2021-09-20 11:44:07 -0500

    Changed from REACT to REACT+Redux

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import { Provider, connect } from 'react-redux';
 import { createStore } from 'redux';
 import './style.css';
 
+// Action creator
 function incrementCounter(num) {
-  return {
-    type: 'INCREMENT',
-    num: num
-  }
+  return { type: 'INCREMENT', num: num };
 }
 
 const initialState = {
-  count: 0
+  count: 0,
 };
-
+// Reducer function
 function reducer(state = initialState, action) {
-  switch(action.type) {
+  switch (action.type) {
     case 'INCREMENT':
       return { count: state.count + action.num };
     default:
@@ -24,24 +22,34 @@ function reducer(state = initialState, action) {
   }
 }
 
-function Counter() {
-  const [counter, setCounter] = useState(0);
-
-  function increment() {
-    setCounter(counter+1);
+function Counter(props) {
+  function handleClick() {
+    props.incrementCounter(1);
   }
-  return <div>
-  <p>{counter}</p>
-  <button onClick={increment}>Increment</button>
-  </div>;
+    return <div>
+    <p>{props.count}</p>
+    <button onClick={handleClick}>Increment</button>
+    </div>;
 }
+
+function mapStateToProps(state) {
+  return {
+    count: state.count,
+  };
+}
+const mapDispatchToProps = {
+  incrementCounter,
+};
 
 const store = createStore(reducer);
 
-const el = <Provider store={store}>
-    <Counter/>
-  </Provider>; 
-ReactDOM.render(
-  el, 
-  document.getElementById('root')
+const Counter2 = connect(mapStateToProps, mapDispatchToProps)(Counter);
+
+const el = (
+  <Provider store={store}>
+    <Counter2/>
+  </Provider>
 );
+
+
+ReactDOM.render(el, document.getElementById('root'));
